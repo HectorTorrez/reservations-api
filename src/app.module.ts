43dadb8d78cc5e847fc53bdced57820256a3da89
HectorTorrez <torrezhectorb@gmail.com>
2026-01-28 19:34:17 -0600

    Implement Property module with CRUD operations, update User and Property models, and add migration for 'enabled' field in Property. Include DTOs and mappers for property handling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { PropertyModule } from './property/property.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: '.env',
     }),
     PrismaModule,
+    PropertyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/property/dto/create-property.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreatePropertyDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  hostId: number;
+}
